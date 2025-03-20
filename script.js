f705f9d9a084f0c6f393ff16596d6ad35f36c27f
Durga-velu <durgadevi134babu@gmail.com>
2025-03-21 00:40:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 function change() {
 let cont=document.querySelector('.new')
 let word=document.querySelector('p')
-cont.removeChild(word)
+
 	let ans=document.createElement('h1')
-ans.innerHTML="Entered Metaverse";
+word.innerHTML="Entered Metaverse";
 	ans.id=$h1;
 	cont.appendChild($h1)
 	
