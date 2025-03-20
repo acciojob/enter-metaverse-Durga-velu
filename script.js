13498ae26fcd0d264456f988fd245ccde4ae75b3
Durga-velu <durgadevi134babu@gmail.com>
2025-03-21 00:39:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function change() {
+let cont=document.querySelector('.new')
+let word=document.querySelector('p')
+cont.removeChild(word)
+	let ans=document.createElement('h1')
+ans.innerHTML="Entered Metaverse";
+	ans.id=$h1;
+	cont.appendChild($h1)
+	
+}
+
+
