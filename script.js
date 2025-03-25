fb7e6a8c39fae6addd1a4b21f3813eca98a5a3f3
Durga-velu <durgadevi134babu@gmail.com>
2025-03-25 23:01:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function change() {
 let word=document.querySelector('#status')
    let ptag =word.parentNode;
 	let ans=document.createElement('h1')
-	ans.textContent=ptag.textContent
+	ans.innerHTML=ptag.innerHTML
 ans.innerHTML="Entered Metaverse";
 	ans.id=word.id
 	ptag.replaceChild(ans,word);
